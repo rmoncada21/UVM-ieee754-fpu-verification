2219aac3e5a1cab316b2f193d20eaf3a7e7c5724
redHatVMw <106123923+rmoncada21@users.noreply.github.com>
2026-05-20 15:15:21 -0600

    chore(filelist): add fpu_env_pkg address
    	modified:   scripts/filelist.f

--- scripts/filelist.f
+++ scripts/filelist.f
@@ -4,11 +4,12 @@
 +incdir+tb_components/packages
 
 # Incluir DUT
-dut/ALU-FPU-ieee754/fp_alu.sv
+dut/ALU-FPU-ieee754/ALU_FP/fp_alu.sv
 
 # Incluir packages
 tb_components/packages/fpu_pkg.sv
 tb_components/packages/fpu_env_pkg.sv
+tb_components/interface/fpu_if.sv
 
 # Incluir top testbench
 testbench.sv
