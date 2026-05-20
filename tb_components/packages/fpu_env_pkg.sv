d38d35e34525891da8f0aae9af45bb5bae6c2840
redHatVMw <106123923+rmoncada21@users.noreply.github.com>
2026-05-20 15:17:47 -0600

    feat(fpu_env_pkg): add ifdef, ifnedf, define macros
    	modified:   tb_components/packages/fpu_env_pkg.sv

--- tb_components/packages/fpu_env_pkg.sv
+++ tb_components/packages/fpu_env_pkg.sv
@@ -1,9 +1,13 @@
+`ifndef FPU_ENV_PKG
+`define FPU_ENV_PKG
+	package fpu_env_pkg;
+	    `include "uvm_macros.svh"
+	    import uvm_pkg::*;
+	    import fpu_pkg::*;
 
-package fpu_env_pkg;
-    `include "uvm_macros.svh"
-    import uvm_pkg::*;
-    import fpu_pkg::*;
+	    `include "tb_components/packages/msg_macros.svh"
 
-    `include "tb_components/packages/msg_macros.svh"
-
-endpackage
+	endpackage
+	import fpu_env_pkg::*;
+	import uvm_pkg::*;
+`endif
