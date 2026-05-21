25ddca53b177e4f0a4650377c75e40e260135ced
redHatVMw <106123923+rmoncada21@users.noreply.github.com>
2026-05-20 19:59:43 -0600

    feat(fpu_pkg): add fpu_opcode_e & fpu_r_mode_e types
    	modified:   tb_components/packages/fpu_pkg.sv

--- tb_components/packages/fpu_pkg.sv
+++ tb_components/packages/fpu_pkg.sv
@@ -1,11 +1,35 @@
 `ifndef FPU_PKG
 `define FPU_PKG
+    
+    package fpu_types_pkg;
 
-    package fpu_pkg;
+        localparam int C_OP_CODE_WIDTH = 3; // opcode
+        localparam int C_FP_WIDTH = 32; // fp_a_i, fp_b_i, fp_c_i
+        localparam int C_R_MODE = 3; // r_mode
+
 
         parameter p_addr_width = 3; // definir luego en el makefile
 
-    endpackage
+        typedef enum logic [C_OP_CODE_WIDTH-1:0]{
+            FADD = 3'd0;
+            FSUB = 3'd1;
+            FMUL = 3'd2;
+            FMADD = 3'd3;
+            FMSUB = 3'd4;
+            FEQ = 3'd5;
+            FLT = 3'd6;
+            FLE = 3'd7;
+        } fpu_opcode_e;
+
+        typedef enum logic [C_R_MODE-1:0]{
+            RNE = 3'b000,
+            RTZ = 3'b001,
+            RDN = 3'b010,
+            RUP = 3'b011,
+            RMM = 3'b100
+        } fpu_r_mode_e;
+
+    endpackage: fpu_types_pkg;
 
     import fpu_pkg::*;
 
