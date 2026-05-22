e8e31c96e7427f425600f32b8f180ae7a18a904e
redHatVMw <106123923+rmoncada21@users.noreply.github.com>
2026-05-22 11:52:13 -0600

    feat(fpu_env): add build and connect phase
    	modified:   tb_components/uvm_components/fpu_env.sv

--- tb_components/uvm_components/fpu_env.sv
+++ tb_components/uvm_components/fpu_env.sv
@@ -1,4 +1,35 @@
 class fpu_env_c  extends uvm_env;
-	`uvm_components_utils(fpu_env_c);
-	
+	`uvm_component_utils(fpu_env_c)
+	fpu_agent_c fpu_agent;
+	// instanciar el scoreboard cuando este
+
+	virtual fpu_if bif;
+
+	// instanciar el scoreboard cuando este hecho
+
+	// constructor
+	function new(string name="fpu_env_c", uvm_component parent);
+		super.new(name, parent);
+	endfunction: new
+	// ------------------------------------------------------------
+	// Function: build_phase
+	// Instancia: agente, configura el vif y crea el scoreboard.
+	// ------------------------------------------------------------
+	virtual function void build_phase(uvm_phase phase);
+		super.build_phase(phase);
+
+		fpu_agent = fpu_agent_c::type_id::create("fpu_agent", this);
+		// scoreboard
+		// coverage
+
+	endfunction: build_phase
+
+	// connect phase, agent, driver, monitor
+	virtual function void connect_phase(uvm_phase phase);
+		super.connect_phase(phase);
+
+	endfunction: connect_phase
+
+
+
 endclass: fpu_env_c
