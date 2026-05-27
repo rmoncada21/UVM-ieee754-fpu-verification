eed91b036ca071649fcf48cd663636d00ca10383
redHatVMw <106123923+rmoncada21@users.noreply.github.com>
2026-05-27 17:56:33 -0600

    feat(fpu_env): connect monitor and scoreboard via uvm tlm
    	modified:   tb_components/uvm_components/fpu_env.sv

--- tb_components/uvm_components/fpu_env.sv
+++ tb_components/uvm_components/fpu_env.sv
@@ -25,7 +25,7 @@ class fpu_env_c  extends uvm_env;
 
 	endfunction: build_phase
 
-	// connect phase, agent, driver, monitor
+	// connect phase, agent, driver, monitor 
 	virtual function void connect_phase(uvm_phase phase);
 		super.connect_phase(phase);
 		fpu_agent.fpu_monitor.tlm_mon_ap.connect(fpu_scoreboard.tlm_mon_aimp);
