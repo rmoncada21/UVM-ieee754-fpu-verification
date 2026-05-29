b16e563545f19999f4cc554d786970a3499718e0
redHatVMw <106123923+rmoncada21@users.noreply.github.com>
2026-05-28 23:20:55 -0600

    chore(fpu_env): add TODO hook for coverage tlm connection
    	modified:   tb_components/uvm_components/fpu_env.sv

--- tb_components/uvm_components/fpu_env.sv
+++ tb_components/uvm_components/fpu_env.sv
@@ -33,6 +33,7 @@ class fpu_env_c  extends uvm_env;
 	virtual function void connect_phase(uvm_phase phase);
 		super.connect_phase(phase);
 		fpu_agent.fpu_monitor.tlm_mon_ap.connect(fpu_scoreboard.tlm_scb_aimp);
+		// TODO: agregar conexion con el coverage
 	endfunction: connect_phase
 
 
