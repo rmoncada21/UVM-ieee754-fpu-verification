9de6df972e7fed668e710f7ac7c921e477b84467
redHatVMw <106123923+rmoncada21@users.noreply.github.com>
2026-05-31 17:21:34 -0600

    chore(makefile): change ntb_random_sedd=$() -> ntb_random_seed_automatic
    	modified:   scripts/sim_make.mk

--- scripts/sim_make.mk
+++ scripts/sim_make.mk
@@ -1,5 +1,5 @@
 _test_target:
-	echo "$(LOGS_SIM:=.A)"
+	echo "$(LOGS_SIM:=.UVM)"
 
 _testbench_sim:
 	./testbench_sim \
@@ -8,9 +8,9 @@ _testbench_sim:
 
 _sim_fpu_base_test:
 	./testbench_sim \
-	+UVM_TESTNAME=$(@:_sim_%=%) \
+	+UVM_TESTNAME=$(@:_sim_%=%)_c \
 	+UVM_VERBOSITY=$(VERBOSITY) \
-	+nbt_random_seed=$(SEED) \
+	+ntb_random_seed_automatic \
 	-l ../$(LOGS_SIM)/$(@:_sim_%=%).log \
 	| tee ../$(LOGS_TESTS)/$(@:_sim_%=%)_$(shell date +%d_%H%_M%S).log
 
@@ -18,6 +18,6 @@ _sim_fpu_NOMBRE_test:
 	./testbench_sim \
 	+UVM_TESTNAME=$(@:_sim_%=%) \
 	+UVM_VERBOSITY=$(VERBOSITY) \
-	+nbt_random_seed=$(SEED) \
+	+ntb_random_seed_automatic \
 	-l ../$(LOGS_SIM)/$(@:_sim_%=%).log \
 	| tee ../$(LOGS_TESTS)/$(@:_sim_%=%)_$(shell date +%d_%H%_M%S).log \
