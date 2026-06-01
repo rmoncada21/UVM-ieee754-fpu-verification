3bae6262372a4ccae399eb7dfe3d27d55db0657b
redHatVMw <106123923+rmoncada21@users.noreply.github.com>
2026-06-01 15:32:33 -0600

    test(fpu_base_sequence): add body task to start,finish and send items to sequencer
    	modified:   tb_components/sequences/fpu_base_sequences.sv

--- tb_components/sequences/fpu_base_sequences.sv
+++ tb_components/sequences/fpu_base_sequences.sv
@@ -1,15 +1,33 @@
 class fpu_base_sequence_c extends uvm_sequence #(fpu_seq_item_c);
 	`uvm_object_utils(fpu_base_sequence_c);
 
-	rand int num_transactions;
+	rand int num_items;
 
 
-	constraint c_num_transactions {
-		num_transactions inside {[100:200]};
+	constraint c_num_items {
+		num_items inside {[100:200]};
 	}
 
 	function new(string name="fpu_base_sequence_c");
 		super.new(name);
-	endfunction: new
+	endfunction : new
 
+	virtual task body();
+		`uvm_info(this.get_type_name(),
+			$sformatf("body aleatorio: %0d items", num_items),
+			UVM_LOW)
+		
+		repeat(num_items) begin
+			this.req = fpu_seq_item_c::type_id::create();
+			// se mandan item al sequencer
+			this.start_item(req);
+			if(!req.randomize()) begin
+				`uvm_error(this.get_type_name(),
+					"Fallo en randomize() base sequence")
+			end
+			this.finish_item(req);
+		end
+
+	endtask: body
+	
 endclass: fpu_base_sequence_c
