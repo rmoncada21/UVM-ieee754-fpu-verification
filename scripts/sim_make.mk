fc567ffb42e47f5aacbfc615a69d245b3647d887
redHatVMw <106123923+rmoncada21@users.noreply.github.com>
2026-06-02 11:47:56 -0600

    chore(sim_make): add extern target to run arith normal test
    	modified:   scripts/sim_make.mk

--- scripts/sim_make.mk
+++ scripts/sim_make.mk
@@ -15,10 +15,19 @@ _sim_fpu_base_test:
 	-l ../$(LOGS_SIM)/$(@:_sim_%=%).log \
 	| tee ../$(LOGS_TESTS)/$(@:_sim_%=%)_$(shell date +%d_%H_%M_%S).log
 
-_sim_fpu_NOMBRE_test:
+_sim_fpu_test_arith_normal:
 	./testbench_sim \
-	+UVM_TESTNAME=$(@:_sim_%=%) \
+	+UVM_TESTNAME=$(@:_sim_%=%)_c \
 	+UVM_VERBOSITY=$(VERBOSITY) \
 	+ntb_random_seed_automatic \
 	-l ../$(LOGS_SIM)/$(@:_sim_%=%).log \
 	| tee ../$(LOGS_TESTS)/$(@:_sim_%=%)_$(shell date +%d_%H%__MS_).log \
+
+
+# 	_sim_fpu_NOMBRE_test:
+# 	./testbench_sim \
+# 	+UVM_TESTNAME=$(@:_sim_%=%)_c \
+# 	+UVM_VERBOSITY=$(VERBOSITY) \
+# 	+ntb_random_seed_automatic \
+# 	-l ../$(LOGS_SIM)/$(@:_sim_%=%).log \
+# 	| tee ../$(LOGS_TESTS)/$(@:_sim_%=%)_$(shell date +%d_%H%__MS_).log \
