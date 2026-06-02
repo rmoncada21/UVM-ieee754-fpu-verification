2fa44117ddcabeb16ec42c344173ef5e968e0df0
redHatVMw <106123923+rmoncada21@users.noreply.github.com>
2026-06-02 13:48:52 -0600

    fix(dpu_sequence_arith_normal): fix randomize with sintax "{}"
    	modified:   tb_components/sequences/fpu_sequence_arith_normal.sv

--- tb_components/sequences/fpu_sequence_arith_normal.sv
+++ tb_components/sequences/fpu_sequence_arith_normal.sv
@@ -12,17 +12,16 @@ class fpu_sequence_arith_normal_c extends fpu_base_sequence_c;
             UVM_LOW)
         
         repeat(num_items) begin
-            req = fpu_item_c::type_id::create("req");
+            req = fpu_seq_item_c::type_id::create("req");
             this.start_item(req);
             // TODO: pasar constraints a fpu_seq_item
-            if(!req.randomize()) with {  
+            if(!req.randomize() with {  
                 op_code_i inside {FADD, FSUB, FMUL, FMADD, FMSUB};
                 fp_a_i[30:23] inside {[8'd70 : 8'd184]};
                 fp_b_i[30:23] inside {[8'd70 : 8'd184]};
-                fp_c_i[30:23] inside {[8'd70 : 8'd184]}; 
-            } begin
-                `uvm_error(this.get_type_name(),
-                "falló la aleatorización")
+                fp_c_i[30:23] inside {[8'd70 : 8'd184]}; }
+            ) begin
+                `uvm_error(this.get_type_name(), "falló la aleatorización")
             end
             this.finish_item(req);
         end
