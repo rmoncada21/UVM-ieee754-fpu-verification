3ae05e1f90e4788e54feb4e38fa484587935c8df
Romaf43 <106123923+rmoncada21@users.noreply.github.com>
2026-06-04 09:29:10 -0600

    feat(fpu_scoreboard): update build_phase
    	modificados:     tb_components/uvm_components/fpu_scoreboard.sv

--- tb_components/uvm_components/fpu_scoreboard.sv
+++ tb_components/uvm_components/fpu_scoreboard.sv
@@ -2,7 +2,10 @@ class fpu_scoreboard_c extends uvm_scoreboard;
 	`uvm_component_utils(fpu_scoreboard_c)
 	
 	// uvm_tlm_analysis_fifo ¿?
+	// proviene del monitor, conectado en el env
 	uvm_analysis_imp #(fpu_seq_item_c, fpu_scoreboard_c) tlm_scb_aimp;
+
+
 
 	function new(string name="fpu_scoreboard_c", uvm_component parent);
 		super.new(name, parent);
@@ -11,8 +14,9 @@ class fpu_scoreboard_c extends uvm_scoreboard;
 
 	virtual function void build_phase(uvm_phase phase);
 		super.build_phase(phase);
-		
 		tlm_scb_aimp = new("tlm_scb_aimp", this);
+
+
 
 		`uvm_info(this.get_type_name(),
 				"TLM - uvm_analysis_imp: tlm_scb_aimp creado",
@@ -27,17 +31,17 @@ class fpu_scoreboard_c extends uvm_scoreboard;
 
 	endfunction : start_of_simulation_phase
 
-	// TODO: hacer report phase
-	virtual function void report_phase(uvm_phase phase);
-		super.report_phase(phase);
-	endfunction : report_phase
-
-
 	// TODO: implementar funcion write
 	virtual task write(fpu_seq_item_c item);
 		`uvm_info(this.get_type_name(),
 			" - ",
 			UVM_LOW);
 	endtask
+
+	// TODO: hacer report phase
+	virtual function void report_phase(uvm_phase phase);
+		super.report_phase(phase);
+	endfunction : report_phase
+
 
 endclass: fpu_scoreboard_c
