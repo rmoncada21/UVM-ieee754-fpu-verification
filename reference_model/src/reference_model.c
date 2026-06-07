9c36bb35719d2e3e2b1fca1becb38b14fbc1146c
Romaf43 <106123923+rmoncada21@users.noreply.github.com>
2026-06-07 15:42:43 -0600

    feat(fpu_ref): add softfloat FADD operation
    	modificados:     reference_model/src/reference_model.c

--- reference_model/src/reference_model.c
+++ reference_model/src/reference_model.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "softfloat.h" // librería de berkeley
+// librería de berkeley
+#include "../../third_party/berkeley-softfloat-3/source/include/softfloat.h"
 
 
 void test_dpic(int num){
@@ -20,42 +21,73 @@ enum {
 	OP_FLE   = 7
 };
 
-static uint_fast8_t map_rmode(uint32_t rm){
-    switch (rm) {
+static uint_fast8_t map_round_mode(uint32_t round_mode){
+    switch (round_mode) {
         case 0: return softfloat_round_near_even;   /* RNE */
         case 1: return softfloat_round_minMag;      /* RTZ */
         case 2: return softfloat_round_min;         /* RDN */
         case 3: return softfloat_round_max;         /* RUP */
         case 4: return softfloat_round_near_maxMag; /* RMM */
         default: return softfloat_round_near_even;
+        // cambiar default a RTZ
     }
 }
 
-void fpu_golden (
+void dpi_fpu_reference (
     unsigned int   op_code_i,
     unsigned int   fp_a_i,
     unsigned int   fp_b_i,
     unsigned int   fp_c_i,
     unsigned int   r_mode_i,
     unsigned int  *fp_result_o,
-    unsigned char *flags_o ) 
-{
-	float32_t    a, b, c, z, res;
-    uint_fast8_t fl = 0; // flags
+    unsigned char *flags_o ) {
 
-    a.v = (uint32_t)fp_a_i; // v = uint32_t ; typedef struct { uint32_t v; } float32_t;
-    b.v = (uint32_t)fp_b_i;
-    c.v = (uint32_t)fp_c_i;
-    res.v = 0u;
+	float32_t    fp_a, fp_b, fp_c, fp_r_mode;
+    float32_t    result;
+    uint_fast8_t flag = 0; // flags
+
+    // v = uint32_t ; typedef struct { uint32_t v; } float32_t;
+    fp_a.v   = (uint32_t)fp_a_i;
+    fp_b.v   = (uint32_t)fp_b_i;
+    fp_c.v   = (uint32_t)fp_c_i;
+    result.v = (uint32_t)0;
 
     /* RISC-V detecta tininess después del redondeo */
     softfloat_detectTininess = softfloat_tininess_afterRounding;
 
 	// TODO: // implementar gating del dut
 	switch(op_code_i){
-		default:
-			res.v = 0u;
-			fl = 0;
+        case OP_FADD: //  opcode suma
+            softfloat_roundingMode = map_round_mode(r_mode_i);
+            softfloat_exceptionFlags = 0;
+            result = f32_add(fp_a, fp_b);
+            flag   = softfloat_exceptionFlags;
+            break;
+
+        case OP_FSUB:
+            break;
+
+        case OP_FMUL:
+            break;
+
+        case OP_FMADD:
+            break;
+
+        case OP_FMSUB:
+            break;
+
+        case OP_FEQ:
+            break;
+
+        case OP_FLT:
+            break;
+
+        case OP_FLE:
+            break;
+
+        default:
+			result.v = 0u;
+			flag = 0;
 			break;
 	}
 	return;
