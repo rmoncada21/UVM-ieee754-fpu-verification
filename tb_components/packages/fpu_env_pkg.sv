984a5345062098fc6055bde201c8e806b87bc5e1
Romaf43 <106123923+rmoncada21@users.noreply.github.com>
2026-07-14 10:06:57 -0600

    chore(fpu_env_pkg): comment out fpu_test_* to use base_test
    	modificados:     tb_components/packages/fpu_env_pkg.sv

--- tb_components/packages/fpu_env_pkg.sv
+++ tb_components/packages/fpu_env_pkg.sv
@@ -1,36 +1,35 @@
 `ifndef FPU_ENV_PKG
 `define FPU_ENV_PKG
 	
-	package fpu_env_pkg;
-	    `include "uvm_macros.svh"
-	    import uvm_pkg::*;
-	    import fpu_types_pkg::*;
-
-	    // packages
-	    `include "tb_components/packages/msg_macros.svh"
-
-	    // seq item
-	    `include "tb_components/seq_item/fpu_seq_item.sv"
-
-	    // sequences
-	    `include "tb_components/sequences/fpu_sequencer.sv"
-	    `include "tb_components/sequences/fpu_base_sequences.sv"
-	    `include "tb_components/sequences/fpu_sequence_arith_normal.sv"
-
-	    // uvm component
-	    // `include "tb_components/seq_item/fpu_seq_item.sv"
-	    `include "tb_components/uvm_components/fpu_driver.sv"
-	    `include "tb_components/uvm_components/fpu_monitor.sv"
-	    `include "tb_components/uvm_components/fpu_agent.sv"
-	    `include "tb_components/uvm_components/fpu_scoreboard.sv"
-	    `include "tb_components/uvm_components/fpu_env.sv"
-
-	    // tests
-	    `include "tb_components/tests/fpu_base_test.sv"
-	    `include "tb_components/tests/fpu_test_arith_normal.sv"
-
-
-	endpackage
-	import fpu_env_pkg::*;
+package fpu_env_pkg;
+	`include "uvm_macros.svh"
 	import uvm_pkg::*;
+	import fpu_types_pkg::*;
+
+	// packages
+	`include "tb_components/packages/msg_macros.svh"
+
+	// seq item
+	`include "tb_components/seq_item/fpu_seq_item.sv"
+
+	// sequences
+	`include "tb_components/sequences/fpu_sequencer.sv"
+	`include "tb_components/sequences/fpu_base_sequences.sv"
+	// `include "tb_components/sequences/fpu_sequence_arith_normal.sv"
+
+	// uvm component
+	`include "tb_components/uvm_components/fpu_driver.sv"
+	`include "tb_components/uvm_components/fpu_monitor.sv"
+	`include "tb_components/uvm_components/fpu_agent.sv"
+	`include "tb_components/uvm_components/fpu_scoreboard.sv"
+	`include "tb_components/uvm_components/fpu_env.sv"
+
+	// tests
+	`include "tb_components/tests/fpu_base_test.sv"
+	// `include "tb_components/tests/fpu_test_arith_normal.sv"
+
+
+endpackage
+import fpu_env_pkg::*;
+import uvm_pkg::*;
 `endif
