c45c95363d932115abdf83a93b8ecd0548d8adf4
redHatVMw <106123923+rmoncada21@users.noreply.github.com>
2026-07-24 10:53:20 -0600

    chore: add target to run subnormal test simulation
    	modified:   sim/sim_make.mk

--- sim/sim_make.mk
+++ sim/sim_make.mk
@@ -18,8 +18,8 @@ MOSTRAR_EXE_ABS:
 
 # tests activos; agregar aquí conforme se integren los del testplan
 TESTS_ACTIVOS := fpu_base_test fpu_test_arith_normal fpu_test_flag_arith \
-				 fpu_test_special_spec fpu_test_norm_spec \
-				 fpu_test_rounding fpu_test_cmp
+				 fpu_test_special_spec fpu_test_norm_spec fpu_test_rounding \
+				 fpu_test_cmp fpu_test_subnormal_arith
 
 # knobs de regresión multi-semilla
 NUM_SEEDS ?= 1
@@ -67,8 +67,8 @@ endef
 ################### Ejecutar los tests
 
 all_test: run_fpu_base_test run_fpu_test_arith_normal run_fpu_test_flag_arith \
-		  run_fpu_test_special_spec run_fpu_test_norm_spec \
-		  run_fpu_test_rounding run_fpu_test_cmp
+		  run_fpu_test_special_spec run_fpu_test_norm_spec run_fpu_test_rounding \
+		  run_fpu_test_cmp run_fpu_test_subnormal_arith
 
 # testbench_sim:
 # 	./$(EXE_SIM) \
